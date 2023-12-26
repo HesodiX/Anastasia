560d5678e3d17437c7549a24cd5fb3e14d299fbe
HesodiX <152792032+HesodiX@users.noreply.github.com>
2023-12-26 13:55:32 +0400

    Add files via upload

---
+++ HeaderMenu.js
@@ -0,0 +1,58 @@
+document
+  .querySelector(".header__container-wrapper_but")
+  .addEventListener("click", function () {
+    console.log("вывод");
+
+    let modal = document.querySelector(".header__container-wrapper_modal");
+    let body = document.querySelector("body");
+
+    console.log(modal.style.transform);
+
+    setTimeout(() => {
+      modal.style.transform =
+        modal.style.transform === "translate(0px, -100%)"
+          ? "translate(0, 0%)"
+          : "translate(0px, -100%)";
+    }, 100);
+
+    modal.style.display = "block";
+
+    body.style.overflowY =
+      body.style.overflowY === "hidden" ? "auto" : "hidden";
+  });
+
+window.addEventListener(
+  "resize",
+  function (event) {
+    if (event.target.innerWidth > 500) {
+      let modal = document.querySelector(".header__container-wrapper_modal");
+      let body = document.querySelector("body");
+
+      body.style.overflowY = "auto";
+
+      modal.style.display = "none";
+
+      modal.style.transform = "translate(0px, -100%)";
+    }
+  },
+  true
+);
+
+function init() {
+  new ymaps.Map('map', {
+      center: [46.35513944139531,48.073751633088044],
+      zoom: 17,
+  })
+
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
+
+
+ymaps.ready(init);
